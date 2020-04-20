b837a11db840d634ed9d7b61667b52d3581edaaf
lapengo <mylapengo@gmail.com>
2020-04-21 01:07:31 +0700

    Gate Auth Menu Master

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,13 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'LAPENGO') }}</title>
 
-    
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -25,7 +25,7 @@
     <!-- NProgress -->
     <link href="{{asset('vendors/nprogress/nprogress.css')}}" rel="stylesheet">
 
-    
+
     <!-- bootstrap-daterangepicker -->
     <link href="{{asset('vendors/bootstrap-daterangepicker/daterangepicker.css')}}" rel="stylesheet">
     <!-- bootstrap-datetimepicker -->
@@ -84,22 +84,41 @@
 
                   <li><a href="{{ url('home') }}"><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a></li>
 
-                  <li><a href="{{ url('admin') }}"><i class="fa fa-users"></i> Administrator <span class="fa fa-chevron-down"></span></a></li>
 
-                  <li><a><i class="fa fa-send"></i> Laporan <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="{{route('laporan.index')}}">Data Laporan</a></li> 
-                      <li><a href="{{route('laporan.subdit')}}">Data Laporan Subdit</a></li> 
-                    </ul>
-                  </li>
-                
+                    @can('isUnit')
+                        <li><a><i class="fa fa-send"></i> Laporan <span class="fa fa-chevron-down"></span></a>
+                            <ul class="nav child_menu">
+                                <li><a href="{{route('laporan.index')}}">Data Laporan</a></li>
+                            </ul>
+                        </li>
+                    @endcan
 
-                  <li><a><i class="fa fa-bar-chart"></i> Data Laporan <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="{{route('chart.unit')}}">Data Laporan Subdit</a></li> 
-                      <li><a href="{{route('chart.subdit')}}">Data Laporan Bibnopsal</a></li> 
-                    </ul>
-                  </li>
+                    @can('isAdmin')
+                        <li><a href="{{ url('admin') }}"><i class="fa fa-users"></i> Administrator <span class="fa fa-chevron-down"></span></a></li>
+                    @endcan
+
+                    @can('isSubdit')
+                        <li><a><i class="fa fa-send"></i> Laporan <span class="fa fa-chevron-down"></span></a>
+                            <ul class="nav child_menu">
+                                <li><a href="{{route('laporan.subdit')}}">Data Laporan Subdit</a></li>
+                            </ul>
+                        </li>
+
+
+                        <li><a><i class="fa fa-bar-chart"></i> Data Laporan <span class="fa fa-chevron-down"></span></a>
+                            <ul class="nav child_menu">
+                                <li><a href="{{route('chart.unit')}}">Data Laporan Subdit</a></li>
+                            </ul>
+                        </li>
+                    @endcan
+
+                    @can('isBibnopsal')
+                        <li><a><i class="fa fa-bar-chart"></i> Data Laporan <span class="fa fa-chevron-down"></span></a>
+                            <ul class="nav child_menu">
+                                <li><a href="{{route('chart.unit')}}">Data Laporan Subdit</a></li>
+                            </ul>
+                        </li>
+                    @endcan
 
 
                 </ul>
@@ -143,7 +162,7 @@
                             <img src="{{asset('images/user.png')}}" alt="">{{ Auth::user()->name }}
                             </a>
                             <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
-                                
+
                             {{-- <a class="dropdown-item"  href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a> --}}
 
                             <a class="dropdown-item" href="{{ route('logout') }}"
@@ -158,10 +177,10 @@
 
                             </div>
                         </li>
-                    
+
                     @endguest
-  
-                   
+
+
                 </ul>
               </nav>
             </div>
@@ -177,10 +196,10 @@
                 <div class="title_left">
                   <h3>@yield('title_page')</h3>
                 </div>
-        
-                 
+
+
               </div>
-        
+
               <div class="clearfix"></div>
                     @yield('content')
               </div>
@@ -199,8 +218,8 @@
       </div>
     </div>
 
-    
- 
+
+
 
     <!-- jQuery -->
     <script src="{{asset('vendors/jquery/dist/jquery.min.js')}}"></script>
@@ -209,21 +228,21 @@
     <!-- FastClick -->
     <script src="{{asset('vendors/fastclick/lib/fastclick.js')}}"></script>
     <!-- NProgress -->
-    <script src="{{asset('vendors/nprogress/nprogress.js')}}"></script> 
+    <script src="{{asset('vendors/nprogress/nprogress.js')}}"></script>
 
     <!-- DataTables -->
     <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
 
-    
-    
+
+
     <!-- bootstrap-daterangepicker -->
     <script src="{{asset('vendors/moment/min/moment.min.js')}}"></script>
     <script src="{{asset('vendors/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
-    <!-- bootstrap-datetimepicker -->    
+    <!-- bootstrap-datetimepicker -->
     <script src="{{asset('vendors/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js')}}"></script>
-    
+
     @stack('scripts')
-    
+
     <!-- Custom Theme Scripts -->
     <script src="{{asset('build/js/custom.min.js')}}"></script>
   </body>
